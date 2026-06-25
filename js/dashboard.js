06e5339a669dabdb01e2d0bfd629631b101c7f8b
edvaniocamurcas-stack <edvaniocamurcas@gmail.com>
2026-06-25 18:25:44 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,8 @@
+function carregarDashboard(resumo) {
 
+    preencher("acv-base", moeda(resumo.ACV_Base));
+    preencher("acv-renovado", moeda(resumo.ACV_Renovado));
+    preencher("percentual-renovacao", percentual(resumo.Percentual_Renovacao));
+    preencher("escolas", resumo.Escolas);
+
+}
