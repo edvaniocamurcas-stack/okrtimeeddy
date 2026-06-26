2ccaa612a6d91c20157032838dcddf117306c071
edvaniocamurcas-stack <edvaniocamurcas@gmail.com>
2026-06-25 21:10:25 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,5 +4,25 @@ function carregarDashboard(resumo) {
     preencher("acv-renovado", moeda(resumo.ACV_Renovado));
     preencher("percentual-renovacao", percentual(resumo.Percentual_Renovacao));
     preencher("escolas", resumo.Escolas);
+    
+const radar = gerarRadarExecutivo(dados);
+
+const radarDiv = document.getElementById("radar-executivo");
+
+if(radarDiv){
+
+    radarDiv.innerHTML = radar.map(item=>`
+
+        <div class="alerta">
+
+            <h3>${item.nivel} ${item.titulo}</h3>
+
+            <p>${item.descricao}</p>
+
+        </div>
+
+    `).join("");
+
+}
 
 }
